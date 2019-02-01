367bfaf3cf0aee8eeb3215b9d56e7aa8c9072df6
seeduardo <romenflatiron@gmail.com>
2019-02-01 11:50:29 +0000

    Improvements to rendered tabs styling

--- src/Organ.js
+++ src/Organ.js
@@ -19,16 +19,22 @@ class Organ extends Component {
       <div >
         <img
         className="organ-blowup" onClick={() => handleOrganClick(organ)} alt={organ.organ} src={organ.image}/>
-        {
+        <div className="tab" >{
           organ.tabs.map(
-            tab => <OrganTab handleTabClick={this.handleTabClick} className="tab" clicked={
+            tab => <OrganTab handleTabClick={this.handleTabClick}  clicked={
                 this.state.clickedTab === tab
                 ? "active"
                 : "inactive"
                 }
                 organ={organ} tab={tab} key={tab} />
-          )
-        }
+          )}
+          </div>
+          {this.state.clickedTab
+          ? <div id={organ.id} className="tabcontent">
+              <h3>{this.state.clickedTab}</h3>
+              <p>{this.state.clickedTab} plus some other stuff from some other source innit.</p>
+            </div>
+          :null}
 
       </div>
     );
--- src/OrganTab.js
+++ src/OrganTab.js
@@ -2,26 +2,15 @@ import React, { Component } from 'react';
 
 class OrganTab extends Component {
 
-
-
   render() {
     const {organ, tab, handleTabClick, clicked} = this.props
     return (
-
       <div>
         <button className="tablinks" onClick={() => handleTabClick(tab)}>{tab}</button>
-        {
-          clicked === "active"
-          ? <div id={organ.id} className="tabcontent">
-              <h3>{tab}</h3>
-              <p>{tab} plus some other stuff from some other source innit.</p>
-            </div>
-          :null
-        }
       </div>
-
     );
   }
+
 }
 
 export default OrganTab
