02b0ebdd879cb3689d891d1538108219c4c7eab3
seeduardo <romenflatiron@gmail.com>
2019-02-01 10:31:21 +0000

    Individual tabs in OrganTab working with basic styling

--- src/Organ.js
+++ src/Organ.js
@@ -3,17 +3,33 @@ import OrganTab from './OrganTab'
 
 class Organ extends Component {
 
+  state = {
+    clickedTab: ''
+  }
+
+  handleTabClick = (tab) => {
+    this.setState({
+      clickedTab: tab
+    })
+  }
+
   render() {
-    const {organ} = this.props
+    const {organ, handleOrganClick} = this.props
     return (
       <div >
         <img
-        className="organ-blowup" onClick={() => this.props.handleOrganClick(organ)} alt={organ.organ} src={organ.image}/>
+        className="organ-blowup" onClick={() => handleOrganClick(organ)} alt={organ.organ} src={organ.image}/>
         {
           organ.tabs.map(
-            tab => <OrganTab className="tab" organ={organ} tab={tab} key={tab} />
+            tab => <OrganTab handleTabClick={this.handleTabClick} className="tab" clicked={
+                this.state.clickedTab === tab
+                ? "active"
+                : "inactive"
+                }
+                organ={organ} tab={tab} key={tab} />
           )
         }
+
       </div>
     );
   }
--- src/OrganTab.js
+++ src/OrganTab.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 
 class OrganTab extends Component {
 
-  expandOrganTab = (tab, organ) => {
 
-    console.log(tab)
-
-  }
 
   render() {
-    const {organ, tab} = this.props
+    const {organ, tab, handleTabClick, clicked} = this.props
     return (
 
       <div>
-        <button className="tablinks" onClick={() => this.expandOrganTab(tab, organ)}>{tab}</button>
-        <div id={organ.id} className="tabcontent">
-          <h3>{tab}</h3>
-          <p>{tab} plus some other stuff from some other source innit.</p>
-        </div>
+        <button className="tablinks" onClick={() => handleTabClick(tab)}>{tab}</button>
+        {
+          clicked === "active"
+          ? <div id={organ.id} className="tabcontent">
+              <h3>{tab}</h3>
+              <p>{tab} plus some other stuff from some other source innit.</p>
+            </div>
+          :null
+        }
       </div>
 
     );
