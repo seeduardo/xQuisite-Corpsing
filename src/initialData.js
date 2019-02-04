62acc0c4e096a5a1f54c43f1ce4c6dbd1656ddbd
seeduardo <romenflatiron@gmail.com>
2019-02-04 19:16:44 +0000

    Added images and text to data to expand to 12 organs

--- src/initialData.js
+++ src/initialData.js
@@ -1,21 +1,22 @@
-import brainTn from './Brain_tn.jpg';
-import handTn from './Hand_tn.jpg';
-import gutTn from './Gut_tn.jpg';
-import brain from './Brain.jpg';
-import hand from './Hand.jpg';
-import gut from './Gut.jpg';
+// import brainTn from './Brain_tn.jpg';
+// import handTn from './Hand_tn.jpg';
+// import gutTn from './organImages/Gut_tn.jpg';
+// import brain from './Brain.jpg';
+// import hand from './Hand.jpg';
+// import gut from './Gut.jpg';
 
+const imgUrlRoot = 'http://localhost:3000/organImages/'
 
 const initialData = [
   {
     id: 1,
     organ: "brain",
     title: "Headquartered in the Nerve Centre",
-    image: brain,
-    thumbnail: brainTn,
+    image: `${imgUrlRoot + 'Brain.jpg'}`,
+    thumbnail: `${imgUrlRoot + 'Brain_tn.jpg'}`,
     quotation: "'It is certainly now common knowledge that the brain, even although a physical organ, bears some intimate relation to the mind; and this knowledge is manifestly embodied in common idiom. When referring to admittedly mental happenings or conditions we speak of racking one’s brains, picking another’s brains, having it on the brain, having a good brain or, contrariwise, being all brawn and no brain; and so on.' - CV Borst",
-    bottom: 715,
-    left: 110,
+    bottom: 700,
+    left: 115,
     tabs: [
       {name: "The Body Politic", content: `'The act of neurofeedback gives users an idea, an image, about their selves that is rather computerized. They describe the neurofeedback process as a "defragmentation of your computer", "cubes put in the right order", "a computer wiring me", "the program that reacts on the waves in your head", "my system is unstable", "my system resets itself over and over again". One could say that in the search for a better self the brain becomes an important partner for the user. This brain thinks for, speaks with, works on, and interferes with the user. It can split into neurotransmitters, brainwaves, neurons, substances and alpha-things. It interacts with the user’s world, life, psychology and ideas, but also the other way around. It can withhold the user from her or his real self, or it can change or cure this real self.
       New technologies of the self give rise to new selves, stated Foucault, and in the case of the brain devices this is clearly the case. The act of manipulating the brain with a device makes people very aware of their biological constitution. First they learn that their problems are brain problems, then they see these problems visualized in their brain map, and next they interfere directly with their brains to fix these problems. In this process the brain is clearly separated from the self. But more than that, users of brain devices need more and more entities to explain their selves and their healing process.' - Jonna Brennkinmeijer`},
@@ -48,8 +49,8 @@ const initialData = [
     id: 2,
     organ: "hand",
     title: "Manoeuvre, Manufacture, Manipulate",
-    image: hand,
-    thumbnail: handTn,
+    image: `${imgUrlRoot + 'Hand.jpg'}`,
+    thumbnail: `${imgUrlRoot + 'Hand_tn.jpg'}`,
     quotation: "'Possession of a fully developed hand profoundly transformed the relationship of the human being to its own body, thus altering the relationship between humans and the natural world. The interaction of the hand with the rest of the body brought about self-consciousness and laid the foundations for the unique sense of agency that is experienced by humans.' - of Raymond Tallis",
     bottom: 650,
     left: 250,
@@ -65,11 +66,164 @@ const initialData = [
     id: 3,
     organ: "gut",
     title: "A Hunger and an Instinct",
-    image: gut,
-    thumbnail: gutTn,
+    image: `${imgUrlRoot + 'Gut.jpg'}`,
+    thumbnail: `${imgUrlRoot + 'Gut_tn.jpg'}`,
     quotation: `'Embedded in the expression "gut feeling" is the notion that the gut is the first part of the body to respond to the environment, producing an emotional sixth sense that governs rational thought processes. This concept is not so foreign to our current physiological models in which the intestinal tract from mouth to anus is described as an exterior organ that mediates between the outside world and the internal viscera, with the intestinal epithelium providing an immunological gateway between self and other.' - M Mathias and AM Moore`,
     bottom: 960,
     left: 60,
+    tabs: [
+      {name: "The Body Politic", content: "blah"},
+      {name: "Out of Body Experience", content: "blah2"},
+      {name: "Corpus Delicti", content: "blah3"},
+      {name: "Foreign Body", content: "blah4"},
+      {name: "The Body Text", content: "blah5"},
+    ]
+  },
+  {
+    id: 4,
+    organ: "arm",
+    title: "Embrace the Change",
+    image: `${imgUrlRoot + 'Arm.jpg'}`,
+    thumbnail: `${imgUrlRoot + 'Arm_tn.jpg'}`,
+    quotation: `'Something about the arm.' - Someone`,
+    bottom: 1240,
+    left: 170,
+    tabs: [
+      {name: "The Body Politic", content: "blah"},
+      {name: "Out of Body Experience", content: "blah2"},
+      {name: "Corpus Delicti", content: "blah3"},
+      {name: "Foreign Body", content: "blah4"},
+      {name: "The Body Text", content: "blah5"},
+    ]
+  },
+  {
+    id: 5,
+    organ: "eye",
+    title: "Foresighted Blindness to a Panopticism",
+    image: `${imgUrlRoot + 'Eye.jpg'}`,
+    thumbnail: `${imgUrlRoot + 'Eye_tn.jpg'}`,
+    quotation: `'Something about the eye.' - Someone`,
+    bottom: 1705,
+    left: 90,
+    tabs: [
+      {name: "The Body Politic", content: "blah"},
+      {name: "Out of Body Experience", content: "blah2"},
+      {name: "Corpus Delicti", content: "blah3"},
+      {name: "Foreign Body", content: "blah4"},
+      {name: "The Body Text", content: "blah5"},
+    ]
+  },
+  {
+    id: 6,
+    organ: "foot",
+    title: "Tread Lightly, Wanderer",
+    image: `${imgUrlRoot + 'Foot.jpg'}`,
+    thumbnail: `${imgUrlRoot + 'Foot_tn.jpg'}`,
+    quotation: `'Something about the foot.' - Someone`,
+    bottom: 1350,
+    left: 95,
+    tabs: [
+      {name: "The Body Politic", content: "blah"},
+      {name: "Out of Body Experience", content: "blah2"},
+      {name: "Corpus Delicti", content: "blah3"},
+      {name: "Foreign Body", content: "blah4"},
+      {name: "The Body Text", content: "blah5"},
+    ]
+  },
+  {
+    id: 7,
+    organ: "heart",
+    title: "Core Zone Fluid Mechanics",
+    image: `${imgUrlRoot + 'Heart.jpg'}`,
+    thumbnail: `${imgUrlRoot + 'Heart_tn.jpg'}`,
+    quotation: `'Something about the heart.' - Someone`,
+    bottom: 2030,
+    left: 70,
+    tabs: [
+      {name: "The Body Politic", content: "blah"},
+      {name: "Out of Body Experience", content: "blah2"},
+      {name: "Corpus Delicti", content: "blah3"},
+      {name: "Foreign Body", content: "blah4"},
+      {name: "The Body Text", content: "blah5"},
+    ]
+  },
+  {
+    id: 8,
+    organ: "leg",
+    title: "The Stance of Legend",
+    image: `${imgUrlRoot + 'Leg.jpg'}`,
+    thumbnail: `${imgUrlRoot + 'Leg_tn.jpg'}`,
+    quotation: `'Something about the leg.' - Someone`,
+    bottom: 1995,
+    left: 60,
+    tabs: [
+      {name: "The Body Politic", content: "blah"},
+      {name: "Out of Body Experience", content: "blah2"},
+      {name: "Corpus Delicti", content: "blah3"},
+      {name: "Foreign Body", content: "blah4"},
+      {name: "The Body Text", content: "blah5"},
+    ]
+  },
+  {
+    id: 9,
+    organ: "nose",
+    title: "Old Factory of Sense",
+    image: `${imgUrlRoot + 'Nose.jpg'}`,
+    thumbnail: `${imgUrlRoot + 'Nose_tn.jpg'}`,
+    quotation: `'Something about the nose.' - Someone`,
+    bottom: 2690,
+    left: 65,
+    tabs: [
+      {name: "The Body Politic", content: "blah"},
+      {name: "Out of Body Experience", content: "blah2"},
+      {name: "Corpus Delicti", content: "blah3"},
+      {name: "Foreign Body", content: "blah4"},
+      {name: "The Body Text", content: "blah5"},
+    ]
+  },
+  {
+    id: 10,
+    organ: "rib",
+    title: "Like a Cage for Laughter",
+    image: `${imgUrlRoot + 'Rib.jpg'}`,
+    thumbnail: `${imgUrlRoot + 'Rib_tn.jpg'}`,
+    quotation: `'Something about the rib.' - Someone`,
+    bottom: 2810,
+    left: 35,
+    tabs: [
+      {name: "The Body Politic", content: "blah"},
+      {name: "Out of Body Experience", content: "blah2"},
+      {name: "Corpus Delicti", content: "blah3"},
+      {name: "Foreign Body", content: "blah4"},
+      {name: "The Body Text", content: "blah5"},
+    ]
+  },
+  {
+    id: 11,
+    organ: "spine",
+    title: "Articulate and Fearless",
+    image: `${imgUrlRoot + 'Spine.jpg'}`,
+    thumbnail: `${imgUrlRoot + 'Spine_tn.jpg'}`,
+    quotation: `'Something about the spine.' - Someone`,
+    bottom: 2965,
+    left: 75,
+    tabs: [
+      {name: "The Body Politic", content: "blah"},
+      {name: "Out of Body Experience", content: "blah2"},
+      {name: "Corpus Delicti", content: "blah3"},
+      {name: "Foreign Body", content: "blah4"},
+      {name: "The Body Text", content: "blah5"},
+    ]
+  },
+  {
+    id: 12,
+    organ: "throat",
+    title: "So Art to Swallow",
+    image: `${imgUrlRoot + 'Throat.jpg'}`,
+    thumbnail: `${imgUrlRoot + 'Throat_tn.jpg'}`,
+    quotation: `'Something about the throat.' - Someone`,
+    bottom: 3355,
+    left: 70,
     tabs: [
       {name: "The Body Politic", content: "blah"},
       {name: "Out of Body Experience", content: "blah2"},
