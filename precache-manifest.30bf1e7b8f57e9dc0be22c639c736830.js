96b7b8b33c40b4537d3c796fc34406a382de65bc
seeduardo <romenflatiron@gmail.com>
2019-03-07 00:30:57 +0000

    Updates

--- precache-manifest.30bf1e7b8f57e9dc0be22c639c736830.js
+++ precache-manifest.30bf1e7b8f57e9dc0be22c639c736830.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6fa8f9f11b840591918b",
+    "revision": "aed75f4627e412b12620",
     "url": "/xQuisite-Corpsing-FrontEnd/static/css/main.90bd7ba5.chunk.css"
   },
   {
-    "revision": "6fa8f9f11b840591918b",
-    "url": "/xQuisite-Corpsing-FrontEnd/static/js/main.6fa8f9f1.chunk.js"
+    "revision": "aed75f4627e412b12620",
+    "url": "/xQuisite-Corpsing-FrontEnd/static/js/main.aed75f46.chunk.js"
   },
   {
     "revision": "7f50a2bc032c0b4d9cf1",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/xQuisite-Corpsing-FrontEnd/static/js/runtime~main.e79baadc.js"
   },
   {
-    "revision": "ce6338ef704b962a659f0abc38e518b3",
+    "revision": "ae31df4a62e9542c38ac7465b3b58e31",
     "url": "/xQuisite-Corpsing-FrontEnd/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/xQuisite-Corpsing-FrontEnd/precache-manifest.700e162336195beb4f74a0a14e32abd0.js"
+  "/xQuisite-Corpsing-FrontEnd/precache-manifest.30bf1e7b8f57e9dc0be22c639c736830.js"
 );
 
 workbox.clientsClaim();
